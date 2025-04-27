c2df67de630c861e3e5698a929ab816aaa227c9d
Harrison Lisle <hlisle@protonmail.com>
2025-04-27 17:08:54 +0800

    Set up Environment for DB connection Created a .env file for database connections as well as a connection to a SQLAlchemy database.

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv("SQLALCEHMY_TRACK_MODIFICATIONS").lower() in ("true", 1)
+
+db = SQLAlchemy(app)
 
 # Home Page
 @app.route('/')
